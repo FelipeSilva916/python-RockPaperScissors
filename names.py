5d6ef6be18cd00554da5ee14d50e1527c324c0a0
Felipe Silva <felipesilva916@gmail.com>
2022-08-14 13:26:15 -0700

    Update names.py

--- names.py
+++ names.py
@@ -8,13 +8,46 @@
 # newTuple = names + ('Mick', 'Keith')
 # print(newTuple)
 
-# =============================================
+# =================== Library  ==========================
 
-dog = {'name': 'Roger', 'age': 5, 'weight': '20 lbs'}
+# dog = {'name': 'Roger', 'age': 5, 'weight': '20 lbs'}
 
-print(dog['name'])
-print(dog['age'])
+# print(dog['name'])
+# print(dog['age'])
 
-dog['name'] = 'Rocky'
-print(dog)
-print(dog.get('name'))
+# dog['name'] = 'Rocky'
+# print(dog)
+# print(dog.get('name'))
+# print('color' in dog)
+
+# print(dog.keys())
+# print(dog.values())
+# print(dog.items())
+
+# dog['favorite food'] = 'chicken'
+# print(dog)
+
+# del dog['age']
+# print(dog)
+
+# ======================= SETS ======================
+# sets cannot have duplicates
+
+# set1 = {'Sydney', 'Melbourne', 'Brisbane', 'Perth'}
+# set2 = {'Paris', 'London', 'New York', 'Sydney'}
+
+# intersect = set1 & set2  # & is the intersection operator
+# print(intersect)  # {'Sydney'}
+
+# mod = set1 | set2  # | is the union operator
+# # {'Sydney', 'Melbourne', 'Brisbane', 'Perth', 'Paris', 'London', 'New York'}
+# print(mod)
+
+# dif = set1 - set2  # - is the difference operator
+# print(dif)  # {'Brisbane', 'Perth'}
+
+
+# =================== FUNCTIONS ======================
+
+def hello():
+    print('Hello World!')
