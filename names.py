712e9d451c369c67197077a584043116cdbf041a
Felipe Silva <felipesilva916@gmail.com>
2022-08-14 13:18:23 -0700

    Create names.py

---
+++ names.py
@@ -0,0 +1,20 @@
+# names = ('John', 'Paul', 'George', 'Ringo')
+
+# print(names[-1])
+# print(len(names))
+# print(names.index('George'))
+# print('Ringo' in names)
+# print(sorted(names))
+# newTuple = names + ('Mick', 'Keith')
+# print(newTuple)
+
+# =============================================
+
+dog = {'name': 'Roger', 'age': 5, 'weight': '20 lbs'}
+
+print(dog['name'])
+print(dog['age'])
+
+dog['name'] = 'Rocky'
+print(dog)
+print(dog.get('name'))
