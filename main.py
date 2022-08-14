075a989652127d2ebd77443e7be55cf7f4133fe0
Felipe Silva <felipesilva916@gmail.com>
2022-08-13 19:11:12 -0700

    get choices

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 
+from operator import ge
+from random import choices
+
+
 def get_choices():
     """
     Asks the user for a choice and returns it.
     """
     player_choice = 'rock'
     computer_choice = 'paper'
-    print('You chose {} and the computer chose {}.'.format(player_choice, computer_choice))
+
     return player_choice, computer_choice
+
+
+choices = get_choices()
+print(choices)
