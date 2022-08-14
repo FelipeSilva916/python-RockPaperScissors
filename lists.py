8fe401e9ab8441d20f5a64619287e3c69926566d
Felipe Silva <felipesilva916@gmail.com>
2022-08-14 13:10:52 -0700

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,12 @@
+dogs = ['Roger', 'Buddy', 'Barkley', 'Buster', 'Baxter', 1, True, 'Dog']
+
+# print('Roger' in dogs)
+# print(dogs[4])
+# print(dogs)
+dogs[5] = 'Beau'
+# print(dogs)
+
+# print(dogs[2:4])  # [2:4] = [2, 3]
+# print(dogs[:4])  # [:4] = [0, 1, 2, 3]
+dogs.append('Rocky')
+print(dogs)
